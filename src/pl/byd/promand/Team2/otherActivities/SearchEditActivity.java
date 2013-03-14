740df4ba80fdd1b18ee8f67f955f10b77e01a1ef
viperjuga <viperjuga13@inbox.lv>
2013-03-14 17:02:37 +0100

    added spinnerlistener (enable/disable) button add)

--- src/pl/byd/promand/Team2/otherActivities/SearchEditActivity.java
+++ src/pl/byd/promand/Team2/otherActivities/SearchEditActivity.java
@@ -2,6 +2,10 @@ package pl.byd.promand.Team2.otherActivities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.Spinner;
 import com.promand.Team2.R;
 
 /**
@@ -12,9 +16,28 @@ import com.promand.Team2.R;
  * To change this template use File | Settings | File Templates.
  */
 public class SearchEditActivity extends Activity {
+    Spinner spinner;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search);
+        spinner = (Spinner)findViewById(R.id.sp_type);
+        spinner.setOnItemSelectedListener(spinnerListener);
     }
+    private  AdapterView.OnItemSelectedListener spinnerListener = new AdapterView.OnItemSelectedListener(){
+        public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+            Object item = parent.getItemAtPosition(pos);
+            if(item.equals("Visit")){
+                Button btn = (Button)findViewById(R.id.btn_add);
+                btn.setEnabled(false);
+            }
+            else {
+                Button btn = (Button)findViewById(R.id.btn_add);
+                btn.setEnabled(true);
+            }
+        }
+        public void onNothingSelected(AdapterView<?> parent) {
+        }
+
+    };
 }
