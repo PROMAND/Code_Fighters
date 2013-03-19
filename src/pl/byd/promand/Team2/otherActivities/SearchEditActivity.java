dc1d5d7957714e297f5867109a5fc32d0abfbb86
viperjuga <viperjuga13@inbox.lv>
2013-03-19 14:53:50 +0100

    Andrew

--- src/pl/byd/promand/Team2/otherActivities/SearchEditActivity.java
+++ src/pl/byd/promand/Team2/otherActivities/SearchEditActivity.java
@@ -5,13 +5,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.Button;
-import android.widget.Spinner;
+import android.widget.*;
 import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
 import com.promand.Team2.R;
+
+import java.util.ArrayList;
 
 public class SearchEditActivity extends SherlockActivity {
     Spinner spinner;
@@ -119,4 +119,42 @@ public class SearchEditActivity extends SherlockActivity {
         getSupportMenuInflater().inflate(R.menu.menu, menu);
         return true;
     }
+    @Override
+    public void onResume(){
+        if(String.valueOf(spinner.getSelectedItem()).equals("Patient") && SearchingResultActivity.takePatient!=null){
+        ArrayList<String> generalList = new ArrayList<String>();
+        generalList.add("Name: " + String.valueOf(SearchingResultActivity.takePatient.get("name")));
+        generalList.add("Surname: " + String.valueOf(SearchingResultActivity.takePatient.get("surname")));
+        generalList.add("Sex: " + String.valueOf(SearchingResultActivity.takePatient.get("sex")));
+        generalList.add("Date of birth: " + String.valueOf(SearchingResultActivity.takePatient.get("date_of_birth")));
+        generalList.add("Pesel: " + String.valueOf(SearchingResultActivity.takePatient.get("pesel")));
+        generalList.add("Country: " + String.valueOf(SearchingResultActivity.takePatient.get("country")));
+        generalList.add("State: " + String.valueOf(SearchingResultActivity.takePatient.get("state")));
+        generalList.add("City: " + String.valueOf(SearchingResultActivity.takePatient.get("town")));
+            generalList.add("Postal code: " + String.valueOf(SearchingResultActivity.takePatient.get("postal_code")));
+            generalList.add("Street: " + String.valueOf(SearchingResultActivity.takePatient.get("street")));
+            generalList.add("Number of house: " + String.valueOf(SearchingResultActivity.takePatient.get("number_of_house")));
+            generalList.add("Number of flat: " + String.valueOf(SearchingResultActivity.takePatient.get("number_of_flat")));
+            generalList.add("Phone: " + String.valueOf(SearchingResultActivity.takePatient.get("phone")));
+            generalList.add("E-mail: " + String.valueOf(SearchingResultActivity.takePatient.get("email")));
+        ArrayAdapter adapter=new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                generalList);
+        ListView lv = (ListView)findViewById(R.id.lv_result);
+       /* lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //String name= generalList.get(position);
+              ////  takeVisit = null;
+              //  takePatient = null;
+              //  takePayer = listResult.get(position);
+                //String pesel = String.valueOf(temp.get("pesel"));
+              //  SearchingResultActivity.this.onDestroy();
+                // Toast.makeText(SearchingResultActivity.this, pesel, 1000).show();
+            }
+        }); */
+        lv.setAdapter(adapter);
+
+    }
+        super.onResume();
+   }
 }
